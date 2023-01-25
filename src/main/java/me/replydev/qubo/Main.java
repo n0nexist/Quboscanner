6e7586cc008ed08996de87a8470c20654da51722
nønexist <111337838+n0nexist@users.noreply.github.com>
2023-01-25 18:35:14 +0100

    (1.3.2) fork by www.n0nexist.gq

---
+++ src/main/java/me/replydev/qubo/Main.java
@@ -0,0 +1,8 @@
+package me.replydev.qubo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CLI.init(args);
+    }
+}
---
+++ src/main/java/me/replydev/utils/Statuz.java
@@ -0,0 +1,13 @@
+package me.replydev.utils;
+
+import me.replydev.qubo.CLI;
+
+public class Statuz {
+
+    public static void status(){
+        try{
+            System.out.print("\033[31m[\033[0mTHREADS: \033[33m"+CLI.getQuboInstance().getThreads()+"\033[31m]-[\033[0mCURRENT: \033[33m"+CLI.getQuboInstance().getCurrent()+"\033[31m]     \r");
+        }catch(Exception a){a.printStackTrace();System.exit(0);}
+    }
+
+}
